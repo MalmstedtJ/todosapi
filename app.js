f329aee0ac6e77595a321778c880195d972e512e
Jakob Malmstedt <jakobmalmstedt@Jakobs-MacBook-Pro.local>
2015-05-26 18:25:13 +0200

    Allow cross domain

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var users = require('./routes/users');
 var todos = require('./routes/todos');
 var mongoose = require('mongoose');
 var fs = require('fs');
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
 
 var app = express();
 
@@ -18,6 +25,7 @@ app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
