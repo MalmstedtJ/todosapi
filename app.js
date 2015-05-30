e045d6472082a332017cc1b046a19087ac181822
Jakob Malmstedt <jakobmalmstedt@Jakobs-MacBook-Pro.local>
2015-05-30 14:34:53 +0200

    temporary removed cross domain restriction

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var todos = require('./routes/todos');
 var mongoose = require('mongoose');
 var fs = require('fs');
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://development.vsjgis-app.divshot.io');
+    //res.header('Access-Control-Allow-Origin', 'http://development.vsjgis-app.divshot.io');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
