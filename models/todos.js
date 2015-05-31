4399f6e96be427b3e0f23bbd76fde283bc30275e
Jakob Malmstedt <jakobmalmstedt@Jakobs-MacBook-Pro.local>
2015-05-31 23:24:51 +0200

    refactoring, separated db code from routing code

--- models/todos.js
+++ models/todos.js
@@ -7,3 +7,71 @@ var todoSchema = new Schema({
 });
 
 mongoose.model('todos', todoSchema);
+
+var TODO = mongoose.model('todos');
+
+//Repository functions
+
+function GetAll(callback){
+	TODO.find(function(err, todos){
+    callback(todos);
+  });
+}
+
+function GetById(id, callback){
+	var query = TODO.where({_id: id});
+	query.findOne(function (err, todo) {
+		if(todo) {
+			callback(todo);
+		}
+		else{callback(err)}
+	});
+}
+
+function Add(description, callback){
+  if (description != '' && description.length <= 80)
+	{
+		var newtodo = new TODO({description: description, rating: '0'});
+		newtodo.save();
+		callback(true);
+	}
+	else{
+		callback(false);
+	}
+}
+
+function Delete(id, callback){
+	var query = TODO.where({_id: id});
+	query.findOne(function (err, todo) {
+		if(todo) {
+			todo.remove();
+			callback(err, true);
+		}
+		else{callback(err, false)}
+	});
+}
+
+function ChangeTodoRating(id, direction, callback){
+	var query = TODO.where({_id: id});
+	query.findOne(function (err, todo) {
+	if(todo) {
+		if(direction === 'down'){
+			todo.rating--;
+			todo.save();
+			callback(true);
+		}
+		else if(direction === 'up'){
+			todo.rating++;
+			todo.save();
+			callback(true);
+		}
+	}
+	else {callback(false)}
+	});
+}
+
+module.exports.GetAll = GetAll;
+module.exports.GetById = GetById;
+module.exports.Add = Add;
+module.exports.Delete = Delete;
+module.exports.ChangeTodoRating = ChangeTodoRating;
--- routes/todos.js
+++ routes/todos.js
@@ -2,154 +2,74 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var todos = require('../models/todos');
-var todos = require('../models/todoRates');
-var ToDo = mongoose.model('todos');
-var rate = mongoose.model('todoRates')
-var async = require('async');
-
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+var todoRates = require('../models/todoRates');
 
 //Get all todos
 router.get('/', function(req, res) {
-  mongoose.model('todos').find(function(err, todos){
-    res.send(todos);
-  });
+	todos.GetAll(function(data){
+		res.send(data);
+	});
 });
 
-//Get all todos
+//Get all downrates
 router.get('/downrates', function(req, res) {
-  mongoose.model('todoRates').find(function(err, rates){
-    res.send(rates);
-  });
+	todoRates.GetAll(function(data){
+		res.send(data);
+	});
 });
 
 //Get todo by id
 router.get('/:id', function(req, res) {
 	var id = req.params.id;
-	var ToDo = mongoose.model('todos');
-	var query = ToDo.where({_id: id});
-	query.findOne(function (err, todo) {
-		if(todo) {
-			res.send(todo);
-		}
-		else{res.send(err)}
+	todos.GetById(id, function(data){
+		res.send(data);
 	});
 });
 
 //Add todo
 router.post('/', function(req, res){
 	var desc = req.body.description;
-	if (desc != '' && desc.length <= 80)
-	{
-		var newtodo = new ToDo({description: desc, rating: '0'});
-		newtodo.save();
-	}
-	else{
-		res.sendStatus(417); //expectation failed
-	}
-	res.sendStatus(200);
+	todos.Add(desc, function(success){
+		var code = success ? 200 : 417;
+		res.sendStatus(code);
+	})
 });
 
 //Delete todo
 router.delete('/:id', function(req, res){
 	var id = req.params.id;
-	var query = ToDo.where({_id: id});
-	query.findOne(function (err, todo) {
-		if(todo) {
-			todo.remove();
-			res.sendStatus(200); 
-		}
+	todos.Delete(id, function(err, success){
+		if(success){res.send(200)}
 		else{res.send(err)}
 	});
 });
 
-//Downrate the specified todo
+//Up rate the specified todo
 router.put('/uprate/:id', function(req, res) {
-	Rate(req, res, 'up');
-});
-
-//Uprate the specified todo
-router.put('/downrate/:id', function(req, res) {
-	Rate(req, res, 'down');
-});
-
-function Rate(request, response, direction)
-{
-	var ip = request.headers['x-forwarded-for'] || 
-     request.connection.remoteAddress || 
-     request.socket.remoteAddress ||
-     request.connection.socket.remoteAddress;
-	var id = request.params.id;
-
-	var status = 200;
-	var query1 = rate.where({todoID: id});
-	var call1 = function(next){
-		query1.findOne(function(err, todoRates){
-		//if there is an existing rating document for this todo
-		if(todoRates){
-			var query = todoRates.ratersDir.filter(function(r){ return r.ip === ip;});
-			var found = query[0];
-			console.log(query);
-			//user has already rated this todo before and in same direction
-			if(found !== undefined && found.direction === direction){
-				status = 304; //not modified
-				next();
-			}
-			//user has already rated this todo but different direction
-			else if(found !== undefined && found.direction !== direction){
-				var index = todoRates.ratersDir.indexOf(found);
-				todoRates.ratersDir.splice(index, 1);
-				todoRates.ratersDir.push({ip: ip, direction: direction});
-				todoRates.save();
-				next();
-			}
-			//the document exist but the user has not rated yet
-			else{
-				todoRates.ratersDir.push({ip: ip, direction: direction});
-				todoRates.save();
-				next();
-			}
-		}
-		//there isn't a rating document for this todo
-		else{
-			var newRate = new rate({todoID: id, ratersDir: [{ip: ip, direction: direction}]});
-			newRate.save();
-			next();
-		}
+	var ip = GetIP(req);
+	var id = req.params.id;
+	todoRates.Rate(id, ip, 'up', function(success){
+		if(success){res.sendStatus(200)}
+		else{res.sendStatus(304)}
 	});
-	}
+});
 
-var call2 = function(next){
-	if(status != 304) {
-		var query2 = ToDo.where({_id: id});
-		query2.findOne(function (err, todo) {
-			if(todo) {
-				if(direction === 'down')
-				{
-					todo.rating--;
-				}
-				else if (direction === 'up')
-				{
-					todo.rating++;
-				}
-			todo.save();
-			status = 200;
-		}
-		else{response.send(err)}
-		});
-	}
-	next();	
-	};
+//Down rate the specified todo
+router.put('/downrate/:id', function(req, res) {
+	var ip = GetIP(req);
+	var id = req.params.id;
+	todoRates.Rate(id, ip, 'down', function(success){
+		if(success){res.sendStatus(200)}
+		else{res.sendStatus(304)}
+	});
+});
 
-var call3 = function(next){
-	response.sendStatus(status);
-	next();
-}
-
-async.series([call1, call2, call3]);
+function GetIP(request){
+	var ip = request.headers['x-forwarded-for'] || 
+    request.connection.remoteAddress || 
+    request.socket.remoteAddress ||
+    request.connection.socket.remoteAddress;
+    return ip;
 }
 
 module.exports = router;
