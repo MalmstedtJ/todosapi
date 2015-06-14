1fc3f5feedd17df1e29da152ccc2a5a440c912b1
Jakob Malmstedt <jakobmalmstedt@Jakobs-MacBook-Pro.local>
2015-06-14 04:45:38 +0200

    bajs

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var allowCrossDomain = function(req, res, next) {
   //res.header('Access-Control-Allow-Origin', 'http://development.vsjgis-app.divshot.io');
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type x-access-token');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
   next();
 }
 var app = express();
