1211a1f7ed03d0ef2a783b0423aa6322daced47a
Jakob Malmstedt <jakobmalmstedt@Jakobs-MacBook-Pro.local>
2015-07-02 13:03:41 +0200

    minor refactoring

--- routes/todos.js
+++ routes/todos.js
@@ -18,7 +18,7 @@ router.post('/', function(req, res){
 		todos.Add(desc, function(success){
 			var code = success ? 200 : 417;
 			res.sendStatus(code);
-			NotifyAdd(req.app.get('wss'));
+			wstools.NotifyAdd(req.app.get('wss'));
 		});
 	}
 	else{res.sendStatus(417)}
@@ -32,7 +32,7 @@ router.delete('/:id', function(req, res){
 			if(success){
 				console.log("Admin user: '"+req.decoded.user+"' just deleted todo with id: '"+id+"'");
 				res.sendStatus(200)
-				NotifyDelete(req.app.get('wss'));
+				wstools.NotifyDelete(req.app.get('wss'));
 			}
 			else{res.send(err)}
 		});
@@ -47,7 +47,7 @@ router.put('/uprate/:id', function(req, res) {
 	todos.Rate(id, ip, 'up', function(success){
 		if(success){
 			res.sendStatus(200);
-			NotifyRate(req.app.get('wss'));
+			wstools.NotifyRate(req.app.get('wss'));
 		}
 		else{res.sendStatus(304)}
 	});
@@ -60,7 +60,7 @@ router.put('/downrate/:id', function(req, res) {
 	todos.Rate(id, ip, 'down', function(success){
 		if(success){
 			res.sendStatus(200);
-			NotifyRate(req.app.get('wss'));
+			wstools.NotifyRate(req.app.get('wss'));
 		}
 		else{res.sendStatus(304)}
 	});	
@@ -72,21 +72,6 @@ function GetIP(request){
     request.socket.remoteAddress ||
     request.connection.socket.remoteAddress;
     return ip;
-}
-
-function NotifyAdd(ws){
-	var msg = {'event': 'todo.add', message: 'A new todo has been added!'}
-	wstools.broadcast(ws, JSON.stringify(msg));
-}
-
-function  NotifyDelete(ws){
-	var msg = {'event': 'todo.delete', message: 'A todo has been deleted!'}
-	wstools.broadcast(ws, JSON.stringify(msg));
-}
-
-function NotifyRate(ws){
-	var msg = {'event': 'todo.rate', message: 'A todo has been rated!'}
-	wstools.broadcast(ws, JSON.stringify(msg));
 }
 
 module.exports = router;
--- tools/WS_UTILS.js
+++ tools/WS_UTILS.js
@@ -5,4 +5,22 @@ function broadcast(wss, message){
 	});
 }
 
+function NotifyAdd(ws){
+	var msg = {'event': 'todo.add', message: 'A new todo has been added!'}
+	broadcast(ws, JSON.stringify(msg));
+}
+
+function  NotifyDelete(ws){
+	var msg = {'event': 'todo.delete', message: 'A todo has been deleted!'}
+	broadcast(ws, JSON.stringify(msg));
+}
+
+function NotifyRate(ws){
+	var msg = {'event': 'todo.rate', message: 'A todo has been rated!'}
+	broadcast(ws, JSON.stringify(msg));
+}
+
 module.exports.broadcast = broadcast;
+module.exports.NotifyAdd = NotifyAdd;
+module.exports.NotifyDelete = NotifyDelete;
+module.exports.NotifyRate = NotifyRate;
